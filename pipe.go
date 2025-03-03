8ea994c0a5666b26f53ddd1c35e3a399d1bfe512
Danno Tripp <danno.tripp@reddit.com>
2025-03-02 18:30:17 -0800

    SHR-570: Add debug message for client
    Signed-off-by: Danno Tripp <danno.tripp@reddit.com>

--- pipe.go
+++ pipe.go
@@ -1409,18 +1409,18 @@ abort:
 }
 
 func syncRead(r *bufio.Reader) (m RedisMessage, err error) {
-	log.Printf("[SHR-570] SR1. SYNC READ FUNC: %v\n", r)
+	log.Printf("[SHR-570] SR1. SYNC READ FUNC: \n")
 next:
 	if m, err = readNextMessage(r); err != nil {
-		log.Printf("[SHR-570] SR2. SYNC READ FUNC: %v\n", r)
+		log.Printf("[SHR-570] SR2. SYNC READ FUNC: \n")
 		return m, err
 	}
-	log.Printf("[SHR-570] SR3. SYNC READ FUNC: %v\n", r)
+	log.Printf("[SHR-570] SR3. SYNC READ FUNC: \n")
 	if m.typ == '>' {
-		log.Printf("[SHR-570] SR4. SYNC READ FUNC: %v\n", r)
+		log.Printf("[SHR-570] SR4. SYNC READ FUNC: \n")
 		goto next
 	}
-	log.Printf("[SHR-570] SR5. SYNC READ FUNC: %v\n", r)
+	log.Printf("[SHR-570] SR5. SYNC READ FUNC: \n")
 	return m, nil
 }
 
