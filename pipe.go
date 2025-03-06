2ffbb362f1b6ea2e1541abd9893d28d592694753
Danno Tripp <danno.tripp@reddit.com>
2025-03-05 21:52:25 -0800

    SHR-570
    Signed-off-by: Danno Tripp <danno.tripp@reddit.com>

--- pipe.go
+++ pipe.go
@@ -1354,7 +1354,7 @@ func (p *pipe) DoCache(ctx context.Context, cmd Cacheable, ttl time.Duration) Re
 		Completed(cmd),
 		cmds.ExecCmd,
 	)
-	log.Printf("[SHR-570] -- DoCache -- resp: %v", resp)
+	log.Printf("[SHR-570] -- DoCache -- len(resp)", len(resp.s))
 	defer resultsp.Put(resp)
 	exec, err := resp.s[4].ToArray()
 	if err != nil {
