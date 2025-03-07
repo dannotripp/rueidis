25a819e4e525845778ba3dcdc1dd5a4b03179890
Danno Tripp <danno.tripp@reddit.com>
2025-03-07 08:34:53 -0800

    SHR-570
    Signed-off-by: Danno Tripp <danno.tripp@reddit.com>

--- pipe.go
+++ pipe.go
@@ -1420,6 +1420,8 @@ func (p *pipe) DoCache(ctx context.Context, cmd Cacheable, ttl time.Duration) Re
 
 	var resp *redisresults
 	var resp2 RedisResult
+	// log the command
+	log.Printf("[SHR-570] DOCACHE() COMMANDs[0]: %v", cmd.Commands()[0])
 	if cmd.Commands()[0] == "CLIENT CACHING YES"{
 		log.Println("[SHR-570] DOCACHE() CLIENT CACHING YES")
 		resp2 = p.Do(
