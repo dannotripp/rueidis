df45ddd7db051bf63f44fc74ee31b831acddca01
Danno Tripp <danno.tripp@reddit.com>
2025-03-06 23:42:21 -0800

    SHR-570
    Signed-off-by: Danno Tripp <danno.tripp@reddit.com>

--- pipe.go
+++ pipe.go
@@ -1373,6 +1373,8 @@ func (p *pipe) DoCache(ctx context.Context, cmd Cacheable, ttl time.Duration) Re
 	v, entry := p.cache.Flight(ck, cc, ttl, now);
 	// log the cache flight result
 	log.Printf("[SHR-570] DOCACHE() CACHE.FLIGHT() RESULT: %v", v.FormatMessage())
+	// log the cache flight entry
+	log.Printf("[SHR-570] DOCACHE() CACHE.FLIGHT() ENTRY: %v", entry)
 	if v.typ != 0 {
 		return newResult(v, nil)
 	} else if entry != nil {
